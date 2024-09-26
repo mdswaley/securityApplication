589b65c64ecdd5ce72354bc11d7474664d18d786
mdswaley <mdswaley075@gmail.com>
2024-09-26 23:04:12 +0530

    Jwt token

--- src/test/java/com/example/securityapplication/SecurityApplicationTests.java
+++ src/test/java/com/example/securityapplication/SecurityApplicationTests.java
@@ -1,13 +1,23 @@
 package com.example.securityapplication;
 
+import com.example.securityapplication.Entity.User;
+import com.example.securityapplication.Service.JWTService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SecurityApplicationTests {
-
+    @Autowired
+    private JWTService jwtService;
     @Test
     void contextLoads() {
+        User user = new User(4L,"adb@gmail.com","123");
+        String token = jwtService.generateToken(user);
+        System.out.println(token);
+
+        Long id = jwtService.getUserIdFromToken(token);
+        System.out.println(id);
     }
 
 }
