d8a486b31fff38f07e66023215f35031afee0024
mdswaley <mdswaley075@gmail.com>
2024-10-03 18:48:48 +0530

    Authentication Exception handling

--- src/main/java/com/example/securityapplication/Filters/JwtAuthFilter.java
+++ src/main/java/com/example/securityapplication/Filters/JwtAuthFilter.java
@@ -8,11 +8,14 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import java.io.IOException;
 @Configuration
@@ -22,32 +25,43 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     private final JWTService jwtService;
     private final UserService userService;
 
+    @Autowired
+    @Qualifier("handlerExceptionResolver")
+    private HandlerExceptionResolver handlerExceptionResolver;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        final String requestTokenHeader = request.getHeader("Authorization");
-        if (requestTokenHeader == null || !requestTokenHeader.startsWith("Bearer")){
-            filterChain.doFilter(request,response);
-            return;
-        }
 
-        String token = requestTokenHeader.split("Bearer ")[1]; //it takes token after Bearer
-        Long userId = jwtService.getUserIdFromToken(token);
+        try {
+            final String requestTokenHeader = request.getHeader("Authorization");
+            if (requestTokenHeader == null || !requestTokenHeader.startsWith("Bearer")) {
+                filterChain.doFilter(request, response);
+                return;
+            }
 
-        if (userId != null && SecurityContextHolder.getContext().getAuthentication() == null){
-            User user = userService.getUserById(userId);
-            UsernamePasswordAuthenticationToken authenticationToken =
-                    new UsernamePasswordAuthenticationToken(user,null,null);
+            String token = requestTokenHeader.split("Bearer ")[1]; //it takes token after Bearer
+            Long userId = jwtService.getUserIdFromToken(token);
+
+            if (userId != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                User user = userService.getUserById(userId);
+                UsernamePasswordAuthenticationToken authenticationToken =
+                        new UsernamePasswordAuthenticationToken(user, null, null);
 
 
 //            some of the web related details authentication like ip address.
-            authenticationToken.setDetails(
-                    new WebAuthenticationDetailsSource().buildDetails(request)
-            );
+                authenticationToken.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request)
+                );
 
-            SecurityContextHolder.getContext().setAuthentication(authenticationToken);;
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                ;
+            }
+
+            filterChain.doFilter(request, response);
+
+        }catch (Exception e){
+            handlerExceptionResolver.resolveException(request,response,null,e);
         }
-
-        filterChain.doFilter(request,response);
 
     }
 }
--- src/main/java/com/example/securityapplication/advice/GlobalExceptionHandler.java
+++ src/main/java/com/example/securityapplication/advice/GlobalExceptionHandler.java
@@ -1,9 +1,11 @@
 package com.example.securityapplication.advice;
 
 import com.example.securityapplication.Exception.ResourceNotFoundException;
+import io.jsonwebtoken.JwtException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -26,6 +28,19 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ApiError> handleAllExceptions(Exception exception) {
         ApiError apiError = new ApiError(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         return new ResponseEntity<>(apiError, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ApiError> handleAllAuthenticationException(AuthenticationException exception){
+        ApiError apiError = new ApiError(exception.getLocalizedMessage(),HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(apiError,HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(JwtException.class)
+    public ResponseEntity<ApiError> handleJwtException(JwtException exception){
+        ApiError apiError = new ApiError(exception.getLocalizedMessage(),HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(apiError,HttpStatus.UNAUTHORIZED);
     }
 
 }
